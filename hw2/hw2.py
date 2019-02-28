72afe644ecfcff19154aef54bfcf4faa75616522
LLipter <katsudonn@163.com>
2019-02-28 10:32:47 -0800

    hw2 done

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,51 +1,70 @@
 import numpy as np
 
-# part 1
+# 1 Include a sec4on with your name
 name = 'Ran Liao'
 extension_id = 'X154149'
 print("I'm " + name + ", and my extension id is " + extension_id + ".")
 
-# part 2
+# 2 Create matrix A with size (3,5) containing random numbers
 A = np.matrix(np.random.rand(3, 5))
 print(A)
 print(type(A))
 
-# part 3
+# 3 Find the size and length of matrix A
 A_size = A.size
 A_shape = A.shape
 print(A_size)
 print(A.shape)
 
-# part 4
+# 4 Resize (crop/slice) matrix A to size (3,4)
 A = A[:, :4]
 print(A)
 
-# part 5
+# 5 Find the transpose of matrix A and assign it to B
 B = A.T
 print(B)
 
-# part 6
+# 6 Find the minimum value in column 1 of matrix B
 b_min = np.min(B[:, 0])
 print(b_min)
 
-# part 7
+# 7 Find the minimum and maximum values for the en4re matrix A
 a_min = np.min(A)
 a_max = np.max(A)
 print(a_min, a_max)
 
-# part 8
+# 8 Create vector X (an array) with 4 random numbers
 X = np.random.rand(4)
 print(X)
 print(type(X))
 
-# part 9/10
+
+# 9 Create a func4on and pass vector X and matrix A in it
+# 10 In the new func4on mul4ply vector X with matrix A and assign the result to D
 def foo(A, X):
     D = A * np.matrix(X).T
     print(D)
-foo(A, X)
+    return D
 
-# part 11
+
+D = foo(A, X)
+
+# 11 Create a complex number Z with absolute and real parts != 0
 Z = complex(np.random.rand(1), np.random.rand(1))
 print(Z)
-print(np.absolute(Z))
 
+# 12 Show its real and imaginary parts as well as it’s absolute value
+print("real part :", Z.real)
+print("imaginary part :", Z.imag)
+print("absolute value :", np.absolute(Z))
+
+# 13 Mul4ply result D with the absolute value of Z and record it to C
+C = D * np.absolute(Z)
+print(C)
+
+# 14 Convert matrix B from a matrix to a string and overwrite B
+B = np.array2string(B)
+print(B)
+
+# 15 Display a text on the screen: ‘Name is done with HW2‘, but pass your ‘Name’ as a string variable 
+print(name, "is done with HW2")
